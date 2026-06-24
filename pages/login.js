380292286a0a3437033c46ec4c11373747002334
biku-cyber <bikashshill662@gmail.com>
2026-06-24 20:15:20 +0530

    Implement login functionality with Firebase auth

---
+++ pages/login.js
@@ -0,0 +1,27 @@
+import { auth } from "./firebase-config.js";
+
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    window.location.href = "courses.html";
+
+  } catch (err) {
+    alert(err.message);
+  }
+});
